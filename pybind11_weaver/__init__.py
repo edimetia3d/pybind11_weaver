fa62514363e58dfb729796d9b06541e4649d4bcc
edimetia3d <edimetia3d@gmail.com>
2022-06-17 09:35:33 +0800

    feat: add cli arg "--get_include"

--- pybind11_weaver/__init__.py
+++ pybind11_weaver/__init__.py
@@ -1,1 +1,7 @@
 __VERSION__ = "0.0.0"
+
+import os.path
+
+
+def get_include():
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), "include")
--- pybind11_weaver/main.py
+++ pybind11_weaver/main.py
@@ -1,15 +1,35 @@
 import argparse
 
+import pybind11_weaver
 from pybind11_weaver import entity
 from pybind11_weaver import gen_unit
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Pybind11 command line interface.")
+def _handle_optional_args(args):
+    if hasattr(args, "get_include") and args.get_include:
+        print(pybind11_weaver.get_include())
+        exit(0)
+
+
+def _add_required_args(parser: argparse.ArgumentParser):
     parser.add_argument("--config",
                         type=str,
                         required=True,
                         help="Path to the config file")
+
+
+def _add_optional_args(parser: argparse.ArgumentParser):
+    parser.add_argument("--get_include",
+                        action="store_true",
+                        default=False,
+                        help="Print the pybind11_weaver include path and exit.")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Pybind11 command line interface.")
+    _add_optional_args(parser)
+    _handle_optional_args(parser.parse_args())
+    _add_required_args(parser)
     return parser.parse_args()
 
 
