76db9562d40322aa6fc644703be6520a8f52d612
edimetia3d <edimetia3d@gmail.com>
2023-10-28 18:19:17 +0800

    chore: update version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 cd $SCRIPT_DIR
-
+rm -rf build
 rm -rf ./dist
 python3 -m pip install --upgrade build twine
 python3 -m build
@@ -12,4 +12,6 @@ if [ "$1" = "upload" ];
   else
     echo Uploading ignored
 fi
+rm -rf build
+rm -rf *.egg-info
 
--- pybind11_weaver/__init__.py
+++ pybind11_weaver/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.1.1"
 
 import os.path
 
